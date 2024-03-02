efaa7cc99b7e9f8b4b42513bc17fb3ae043c6515
lehrmanaidins <lehrmanaidin+github.com@gmail.com>
2024-03-02 07:02:08 -0500

    Second Commit

--- Phase 3/main.py
+++ Phase 3/main.py
@@ -27,6 +27,7 @@ MINOR_ANGLE_RANGE: float = 2.0
 OBJECT_CLOSE_ARMS_AREA: float = 23_500.0
 
 RED_CONTOUR_AREA: float = 12_500.0
+ORANGE_CONTOUR_AREA: float = 12_500.0 #Gonna have to alter this depending on the orange area, probaby gonna be higher bc orange area is bigger
 
 robot = Pitop()
 robot.add_component(DriveController(left_motor_port = "M0", right_motor_port = "M3"))
@@ -52,7 +53,7 @@ def is_object_grabbable(frame):
 
     if (centroid[0] is None or centroid[1] is None):
         robot.miniscreen.display_image(frame)
-        robot.drive.left(0.05, 0)
+        robot.drive.right(0.05, 0)
         return
 
     display_frame = robot_view(frame, image_mask, line_contour, centroid)
@@ -60,11 +61,20 @@ def is_object_grabbable(frame):
 
     contour_area = cv2.contourArea(line_contour)
     if (contour_area > RED_CONTOUR_AREA):
+
+        robot.pincers.open(speed = 50, angle = 60)
+        robot.drive.backward(0.25)
+        sleep(1.0)
+        robot.drive.right(0.25) # Robot turns away as to not detect the red objects on orange block
+        sleep(1.0) #Edit this time to get it at a 90 degree turn so it can look for more red
+        robot.drove.stop_rotation()
+        robot.camera.on_frame = is_object_grabbable
+
         # Close servos to grab object
         # Set some boolean value to true, which then makes the robot look for the oraneg block and drives towards that and opens arms.
         # repeat again
         # 
-        passo900
+
 
     else:
         angle = get_object_target_lock_control_angle((centroid[0] - (frame.shape[1] / 2), centroid[1] - (frame.shape[0] / 2)), frame)
@@ -78,6 +88,46 @@ def is_object_grabbable(frame):
         # print(f'{TURN_SPEED: .2f}, {DRIVE_SPEED: .2f}') 
         robot.drive.robot_move(DRIVE_SPEED * DRIVE_SPEED_FACTOR, TURN_SPEED * TURN_SPEED_FACTOR, robot.drive.wheel_separation / 2)
 
+def drive_to_orange():
+    global robot_has_object_in_arms
+    
+    if (robot_has_object_in_arms):
+        return 
+    
+    frame = ImageFunctions.convert(frame, format="OpenCV")
+
+    image_mask = get_color_mask(frame, 'orange')
+
+    line_contour = find_largest_contour(image_mask)
+
+    centroid = find_centroid(line_contour)
+
+    if (centroid[0] is None or centroid[1] is None):
+        robot.miniscreen.display_image(frame)
+        robot.drive.right(0.05, 0)
+        return
+
+    display_frame = robot_view(frame, image_mask, line_contour, centroid)
+    robot.miniscreen.display_image(display_frame)
+
+    contour_area = cv2.contourArea(line_contour)
+    if (contour_area > ORANGE_CONTOUR_AREA):
+
+        robot.pincers.open(speed = 50, angle = 60)
+        robot.drive.backward(0.25)
+        sleep(1.0)
+        robot.drive.right(0.25) # Robot turns away as to not detect the red objects on orange block
+        sleep(1.0) #Edit this time to get it at a 90 degree turn so it can look for more red
+        robot.drove.stop_rotation()
+        robot.camera.on_frame = is_object_grabbable
+
+        # Close servos to grab object
+        # Set some boolean value to true, which then makes the robot look for the oraneg block and drives towards that and opens arms.
+        # repeat again
+        # 
+     
+
 if __name__ == '__main__':
     robot.camera.on_frame = is_object_grabbable
     pause()
+
